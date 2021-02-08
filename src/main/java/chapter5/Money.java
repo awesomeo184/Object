831dd0bdc8a9a556e349b159b584ec95594afb63
awesomeo184 <awesomeo184@gmail.com>
2021-02-08 17:31:57 +0900

    5.3 MovieType은 영화의 할인 정책 종류를 나열하는 열거형이다.

---
+++ src/main/java/chapter5/Money.java
@@ -0,0 +1,9 @@
+package chapter5;
+
+public class Money {
+
+    public Money minus(Money discountAmount) {
+        return null;
+    }
+
+}
---
+++ src/main/java/chapter5/MovieType.java
@@ -0,0 +1,7 @@
+package chapter5;
+
+public enum MovieType {
+    AMOUNT_DISCOUNT,    //정액 할인 적용
+    PERCENT_DISCOUNT,   //정률 할인 적용
+    NONE_DISCOUNT;      //할인 미적용
+}
