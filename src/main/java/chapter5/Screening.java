0dbdd58846d3b0d22553aa2db5cd9e56efe3f0e4
awesomeo184 <awesomeo184@gmail.com>
2021-02-08 17:16:59 +0900

    5.3 Screening은 상영을 예매할 책임을 갖는다.

---
+++ src/main/java/chapter5/Screening.java
@@ -0,0 +1,17 @@
+package chapter5;
+
+import java.time.LocalDateTime;
+
+public class Screening {
+
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Reservation reserve(Customer customer, int audienceCount) {
+        return null;
+    }
+
+
+
+}
